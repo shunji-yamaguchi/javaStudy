a863f73d83031b3a5dcfba14cc9d6906d5636288
shunji-yamaguchi <shunji.yamaguchi@esm.co.jp>
2017-04-19 17:09:04 +0900

    クラスドキュメントに問題追加、タブをスペースに変更、末尾に改行追加

--- chapter02/example/Calc2.java
+++ chapter02/example/Calc2.java
@@ -1,7 +1,7 @@
 package chapter02.example;
 
 public class Calc2 {
-	public static void main (String[] arg){
-		System.out.println(1 + 2 * 3 - 4 / 2);
-	}
+  public static void main (String[] arg){
+    System.out.println(1 + 2 * 3 - 4 / 2);
+  }
 }
--- chapter02/problem/Problem2_3.java
+++ chapter02/problem/Problem2_3.java
@@ -1,8 +1,15 @@
 package chapter02.problem;
 
-// 問題2-3
+/**
+ * 問題2-3
+ * 上底が2センチ、下底が3センチ、高さが4センチの
+ * 台形の面積を計算するプログラムを作成せよ
+ *
+ * @author shunji.yamaguchi
+ *
+ */
 public class Problem2_3 {
-	public static void main(String[] arg){
-		System.out.println((2 + 3) * 4 /2);
-	}
+  public static void main(String[] arg){
+    System.out.println((2 + 3) * 4 /2);
+  }
 }
