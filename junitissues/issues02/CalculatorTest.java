3707665343dbc8379eb5927eaa9795a19c4ae736
shunji-yamaguchi <shunji.yamaguchi@esm.co.jp>
2017-05-17 11:33:04 +0900

    例外を送出するメソッドのテスト、の解答を追加

---
+++ junitissues/issues02/CalculatorTest.java
@@ -0,0 +1,19 @@
+package junitissues.issues02;
+
+import org.junit.Test;
+
+/**
+ * 【例外を送出するメソッドのテスト】
+ * Calculatorクラスに整数の割り算を行うdivideメソッドを作成し、
+ * 0で割る場合に例外が送出されることを検証するテストを作成せよ。
+ * なお、整数の割り算は小数点以下を切り捨ててよい。
+ *
+ * @author shunji.yamaguchi
+ *
+ */
+public class CalculatorTest {
+    @Test(expected = IllegalArgumentException.class)
+    public void 第２引数に0を指定してdivideを呼び出すとIllegalArgumentExceptionが発生() {
+        new Calculator().divide(1, 0);
+    }
+}
